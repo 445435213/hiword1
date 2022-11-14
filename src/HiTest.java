30af26f0e6e60afed0b3805afb0ddaec9ecbc189
445435213@qq.com <445435213@qq.com>
2022-11-14 14:45:51 +0800

    第2次提交

--- src/HiTest.java
+++ src/HiTest.java
@@ -3,6 +3,7 @@ public class HiTest {
     public static void main(String[] args) {
         System.out.println("hiWord1Test");
         System.out.println("hiWord1Test");
+        System.out.println("hiWord1Test");
     }
 
 }
